6e781c7971c99f666dcef3d6a7e47ebf41909c99
Sergey Rozhnov <bazili073@ya.ru>
2016-12-22 11:28:29 +0400

    add main_guard

---
+++ main_guard.c
@@ -0,0 +1,30 @@
+#ifdef MAIN_GUARD_C
+#else
+#define MAIN_GUARD_C
+
+#include "stm32l1xx_hal.h"
+#include "stm32l151xba.h"
+#include "modem.h"
+#include "my_string.h"
+#include "defines.h"
+#include "EEPROMfunc.h"
+#include "guard_func.h"
+#include "1-Wire.h"
+
+void main_guard(){
+	check_TM();
+	check_inputs();
+	check_battery();
+	check_gsm_message();
+}
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ main_guard.h
@@ -0,0 +1,8 @@
+#ifdef MAIN_GUARD_H
+#else
+#define MAIN_GUARD_H
+
+void main_guard();
+
+
+#endif
